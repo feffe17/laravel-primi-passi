9ccd2f6f8aec580aef262b958ef3765b38a6208f
feffe1702 <fpm.asd@gmail.com>
2025-02-25 17:08:22 +0100

    replace index view with home view and update routes; add home view and styles

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
-});
+    return view('home');
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/shop', function () {
     return view('shop');
-});
+})->name('shop');
